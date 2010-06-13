661065541aacb6e20d2b65556c779ad17f4d92a4
Trace Wax <tracedwax@gmail.com>
2010-06-12 20:34:39 -0400

    Removed compass reference.

--- config/environment.rb
+++ config/environment.rb
@@ -39,8 +39,6 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 
-  # Compass told me to put this here
-  config.gem "compass", :version => ">= 0.10.2"
   config.gem "authlogic"
 
 end
