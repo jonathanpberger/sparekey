ae953a5228ccc62ad1c33bfe506e99bc7d305b23
jonathanpberger <jonathanpberger@gmail.com>
2010-06-13 10:05:37 -0400

    front-end styling WIP

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def logo
+    logo = image_tag("logo.png", :alt => "SpareKeyPlease")
+  end
+
 end
