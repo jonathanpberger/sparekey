a6c17394d6023581ccef50b9608b4334635e378c
Trace Wax <tracedwax@gmail.com>
2010-06-13 12:06:11 -0400

    Progress on friend finding

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,7 +7,7 @@ class MainController < ApplicationController
   require 'open-uri'
   
   def index
-#    load_facebook_feed
+    load_facebook_feed
     
     unless (! params[:user_friend_location] || params[:user_friend_location].empty?)
       @postings = Artifact.find_by_sql ["SELECT artifacts.* FROM artifacts, friends, locations "+
@@ -52,8 +52,9 @@ class MainController < ApplicationController
     
     filtered_items.each do |f|
       #friend = Friend.find f["from"]["id"] 
-      if friend.nil?
-        
+      
+      if Friend.exists? f["from"]["id"]
+      
       end
       
     end
