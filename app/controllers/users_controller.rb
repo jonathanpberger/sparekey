f99680e32653dd9e8d01e3ae544d9ccbcd91caa9
jonathanpberger <jonathanpberger@gmail.com>
2010-06-13 11:40:26 -0400

    somaoeu

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,10 +18,12 @@ class UsersController < ApplicationController
   
   def show
     @user = @current_user
+    @title = @user
   end
 
   def edit
     @user = @current_user
+
   end
   
   def update
