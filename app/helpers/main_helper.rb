ed87a1e3b093a1f52d3d967f7575160a1e2847e6
Paul Blair <ciriwe@phobot.net>
2010-06-13 13:08:29 -0400

    Add tests for filtering facebook friends, and creating friends and locations

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -16,44 +16,23 @@ module MainHelper
     return true if message =~ /^(is|will\s+be)\s+(going\s+to|traveling\s+to|flying\s+to|taking\s+a\s+train\s+to)\s+[A-Z]/
     return true if message =~ /am\s+(going\s+to|traveling\s+to|flying\s+to|taking\s+a\s+train\s+to)\s+[A-Z]/
   end
-  
-  # load the current user's news feed from the Facebook Graph API
-  # remove all entries that don't have anything to do with travel
-  # for all entries that have to do with travel, ensure that the friend is loaded
-  # for the friend, ensure that their location exists, and save the location
-  # save the friend
-  # save the entry as a travel 'artifact' posting
-  def self.load_facebook_feed
-
-    # load the current user's news feed from the Facebook Graph API
-
-    # hardcode token for now
-    token = "2227470867|2.t5FrOE91aDHzk3ETfHW2SQ__.3600.1276444800-218639|WDefUjL5HsE4tKdMbG5cCY_xk7Q."
-     
-    url = "https://graph.facebook.com/me/home?limit=200&access_token=#{token}"
-    buffer = open(URI.encode(url), "UserAgent" => "Ruby-Wget").read
-    result = ActiveSupport::JSON.decode(buffer)
-    items = result['data']
-    logger.debug("load_facebook_feed: items is [#{items.inspect}]")
-    
-    # remove all entries that don't have anything to do with travel
-    # filtered_items = filter_facebook_feed(items)
-    
-    # create dummy filtered items in lieu of filtered hash coming back
-    filtered_items = [{"from"=>{"name"=>"Lee Jones", "id"=>"671290026"}, "id"=>"671290026_131088470242747", "created_time"=>"2010-06-13T12:20:56+0000", "type"=>"status", "updated_time"=>"2010-06-13T12:20:56+0000", "message"=>"Day 2: Exhausted.", "likes"=>3}]
-    
-    filtered_items do |f|
-      friend = Friend.find f["from"]["id"] 
-      if friend.nil?
-        url = "https://graph.facebook.com/me/home?limit=200&access_token=#{token}"
-        
-      end
-      
+ 
+  def self.verify_friend posting
+    friend = Friend.find_or_create_by_social_network_handle posting['from']['name']
+    friend.save
+    friend_info = get_friend_from_facebook(posting['from']['id'])
+    if friend_info['location'] && friend_info['location']['name']
+      location = Location.find_or_create_by_location_name(friend_info['location']['name'])
+      location.save
     end
-    
-    
-    
-    
-  end  
+    new_location = Location.find_by_location_name(friend_info['location']['name'])
+  end
   
+  def self.get_friend_from_facebook id
+
+  end
+
+  def self.create_artifact_from_posting
+    
+  end
 end
