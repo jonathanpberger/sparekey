ce31851318b1e593a093beee951bc9c931c56cf7
Paul Blair <ciriwe@phobot.net>
2010-06-13 10:37:09 -0400

    Remove debugging code

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,7 +3,6 @@ class MainController < ApplicationController
   
   def index
     unless (! params[:user_friend_location] || params[:user_friend_location].empty?)
-$stderr.puts(@postings.inspect)
       @postings = Artifact.find_by_sql ["SELECT artifacts.* FROM artifacts, friends, locations "+
                                         "WHERE artifacts.friend_id = friends.id "+
                                         "AND friends.user_id = ? "+
@@ -11,8 +10,6 @@ $stderr.puts(@postings.inspect)
                                         "AND locations.location_name = ?", 
                                         @current_user.id, 
                                         params[:user_friend_location] ]
-$stderr.puts("USER FRIEND LOCATION IS: " + params[:user_friend_location])                                       
-$stderr.puts(@postings.inspect)
     else
       @postings = @current_user.artifacts.all
     end
